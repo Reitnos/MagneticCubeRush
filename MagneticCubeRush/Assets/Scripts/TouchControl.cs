e1846323a7a730ae6a67915ecc286e8ec9fac0f7
Berat Koçak <42033267+Reitnos@users.noreply.github.com>
2021-08-05 16:10:14 +0300

    Basic player movements and playground

---
+++ MagneticCubeRush/Assets/Scripts/TouchControl.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControl : MonoBehaviour
+{
+    // this script allows player to control the collector by arrow keys
+    // rotating with left and right, moving with up and down keys.
+    
+    [Header("Speed arrangement")]
+    public float moveSpeed = 20.0f;
+    public float rotationSpeed = 200.0f;
+    
+    private bool goingForward = true;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        ArrowkeyMovementControl();
+    }
+
+    private void ArrowkeyMovementControl()
+    {
+        float movement = Input.GetAxis("Vertical") * moveSpeed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed ;
+       
+        // check if player is trying to go back or forward, so that while going back, rotation is arranged accordingly.
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            goingForward = true;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            goingForward = false;
+        }
+
+        if (!goingForward)
+        {
+            rotation = -rotation;
+        }
+
+        rb.velocity = transform.forward * movement;
+        Vector3 eulerRotation = new Vector3(0, rotation, 0);
+        Quaternion deltaRotation = Quaternion.Euler(eulerRotation * Time.fixedDeltaTime) ;
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+}
